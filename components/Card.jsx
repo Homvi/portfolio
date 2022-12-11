2c1c4ea4d782c219ad16945d1036d9bf3e24f39b
HonvedoAdam <honvedoadamkj@gmail.com>
2022-12-11 16:27:30 +0100

    add cards

---
+++ components/Card.jsx
@@ -0,0 +1,45 @@
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { MdWebAsset } from 'react-icons/md';
+import Link from 'next/link'
+
+const Card = () => {
+    return (
+        <div className="rounded-xl hover:scale-[1.01] transition-all duration-75 w-60 h-80 mx-auto  mt-5 bg-gradient-to-r p-1 from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]">
+            <div className="flex flex-col justify-between h-full bg-black text-white rounded-lg p-4">
+                <div className="">
+                    <h4 className="text-lg py-2 font-bold">Wedding site</h4>
+                    <h4 className="text-md py-1 font-bold">description</h4>
+                    <p>
+                        I created this site with the help of a designer friend.
+                    </p>
+                    <div className="">
+                        <h4 className="text-md py-1 font-bold">Technologies</h4>
+                        <p>
+                            React, Next.js, Tailwind, Netlify
+                        </p>
+                    </div>
+
+                </div>
+                <div className="flex justify-around py-2">
+                    <span className='w-5 text-2xl'>
+                        <Link href="#git">
+                            <AiFillGithub />
+                        </Link>
+                    </span>
+                    <span className='w-5 text-2xl'>
+                        <Link href="#git">
+                            <AiFillLinkedin />
+                        </Link>
+                    </span>
+                    <span className='w-5 text-2xl '>
+                        <Link href="#git">
+                            <MdWebAsset />
+                        </Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Introduction from "../components/Introduction";
 import { useEffect, useState } from "react";
+import Card from "../components/Card"
 
 export default function Home() {
   const [isBorderOnNav, setIsBorderOnNav] = useState(false);
@@ -34,23 +35,15 @@ export default function Home() {
             <Introduction />
             <h3
               id="works"
-              className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-white"
+              className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-center  w-full text-white"
             >
               Works and Projects
             </h3>
-            <p>
-              nemo exercitationem cum nihil. Quod provident consequuntur rerum
-              itaque aliquam quasi autem repellendus error tempora, dicta
-              inventore soluta sit fugiat pariatur laboriosam quibusdam, eveniet
-              molestias. Inventore quidem dolore adipisci quae atque deserunt id
-              totam mollitia quo, quos aperiam voluptatem natus similique sed
-              saepe. Dolores voluptates animi, recusandae eum architecto
-              accusantium maiores commodi modi odit praesentium eos est vel
-              reiciendis ab neque eligendi magnam perspiciatis dolore inventore
-              sed ducimus? Suscipit, animi minima quos placeat nemo sunt tenetur
-              veniam asperiores officiis iusto magnam accusamus, unde nostrum
-              voluptates qui sequi. Recusandae, officia dolorem! Recusandae?
-            </p>
+            <div className="flex flex-col  w-full items-center md:flex-row">
+              <Card />
+              <Card />
+              <Card />
+            </div>
           </div>
         </div>
       </div>
