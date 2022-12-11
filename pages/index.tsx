bde12dbab27e9def29d0e352721534620c43f80f
HonvedoAdam <honvedoadamkj@gmail.com>
2022-12-11 14:42:58 +0100

    move introduction to its component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
+
+import Introduction from "../components/Introduction";
 
 export default function Home() {
   return (
@@ -13,28 +14,7 @@ export default function Home() {
         <div className="bg-black text-white">
           <div className="flex flex-col justify-center px-8  min-h-screen bg-black">
             <div className="flex flex-col justify-center items-start max-w-2xl border-gray-700 mx-auto pb-16">
-              <div className="flex flex-col-reverse sm:flex-row items-start">
-                <div className="flex flex-col pr-8">
-                  <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-white">
-                    Adam Honvedo
-                  </h1>
-                  <h2 className="text-gray-200 mb-4">Front-end developer</h2>
-                  <div className="text-gray-400 mb-16">
-                    I build and deploy fast and responsive websites with the
-                    help of React, Next.js, Tailwind and Vercel.
-                  </div>
-                </div>
-                <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
-                  <Image
-                    height={156}
-                    width={158}
-                    src="/img/profile.png"
-                    alt="adam"
-                    className="rounded-full filter grayscale"
-                  />
-                </div>
-              </div>
-
+              <Introduction />
               <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-white">
                 Works and Projects
               </h3>
