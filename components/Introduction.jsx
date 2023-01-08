29492c0a8f913e61bb380f1efe155a64e68263d4
HonvedoAdam <honvedoadamkj@gmail.com>
2023-01-08 20:04:52 +0100

    add padding to intro text

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const Introduction = () => {
     return (
-        <div  className="flex flex-col-reverse sm:flex-row items-start">
+        <div className="flex flex-col-reverse sm:flex-row items-start">
             <div className="flex flex-col pr-8">
                 <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-white">
                     Adam Honvedo
                 </h1>
                 <h2 className="text-gray-200 mb-4">Front-end developer</h2>
-                <div className="text-gray-400 mb-16">
+                <div className="text-gray-400 mb-16 pr-3 md:pr-0">
                     I build and deploy fast and responsive websites with the
                     help of React, Next.js, Tailwind and Vercel.
                 </div>
