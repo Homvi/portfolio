22e9e7f5ea50e5b2b687faee31a586e8b22fb24e
HonvedoAdam <honvedoadamkj@gmail.com>
2023-01-13 20:07:53 +0100

    change git and linkedin icon

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Image from 'next/image'
-import { TiSocialLinkedinCircular, TiSocialGithubCircular } from 'react-icons/ti';
+import { FaGithubAlt, FaLinkedin } from 'react-icons/fa';
+
 
 import Link from 'next/link';
 
@@ -11,14 +11,14 @@ const Contact = () => {
                 id="works"
                 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-left w-full text-white"
             >
-                Contact
+                Find me
             </h3>
-            <div className="flex text-8xl mb-5 justify-start">
-                <a href="https://www.linkedin.com/in/adamhonvedo/" rel="noreferrer noopener"  target="_blank" >
-                    <TiSocialLinkedinCircular />
+            <div className="flex text-6xl mb-5 justify-start">
+                <a href="https://www.linkedin.com/in/adamhonvedo/" rel="noreferrer noopener" className='pr-5 hover:scale-105 text-gray-100 hover:text-white' target="_blank" >
+                    <FaLinkedin />
                 </a>
-                <Link href="https://github.com/Homvi" rel="noreferrer noopener" target="_blank"  >
-                    <TiSocialGithubCircular />
+                <Link href="https://github.com/Homvi" rel="noreferrer noopener" className='hover:scale-105 text-gray-100 hover:text-white' target="_blank"  >
+                    <FaGithubAlt />
                 </Link>
             </div>
         </>
