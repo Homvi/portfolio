3d5a40291deed4a0da35d3e1714d713fb19c07f0
homvi <honvedoadamkj@gmail.com>
2023-05-16 17:42:00 +0200

    add luxcode

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,8 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Introduction from "../components/Introduction";
 import { useEffect, useState } from "react";
-import Card from "../components/Card"
-import Contact from "../components/Contact"
+import Card from "../components/Card";
+import Contact from "../components/Contact";
 
 const cardsData = [
   {
@@ -12,7 +12,8 @@ const cardsData = [
     technologies: "Html, Css, Sass,  Javascript",
     git: "https://github.com/Homvi/akmanSolutions",
     live: "https://akmansolutions.com/",
-    speed: "https://pagespeed.web.dev/report?url=https%3A%2F%2Fakmansolutions.com%2F&form_factor=desktop"
+    speed:
+      "https://pagespeed.web.dev/report?url=https%3A%2F%2Fakmansolutions.com%2F&form_factor=desktop",
   },
   {
     name: "Goldkert transfer",
@@ -20,7 +21,8 @@ const cardsData = [
     technologies: "Html, Css, Sass, Javascript",
     git: "https://github.com/Homvi/GoldkertTransfer",
     live: "https://goldkerttransfer.com/",
-    speed: "https://pagespeed.web.dev/report?url=https%3A%2F%2Fgoldkerttransfer.com%2F&form_factor=desktop"
+    speed:
+      "https://pagespeed.web.dev/report?url=https%3A%2F%2Fgoldkerttransfer.com%2F&form_factor=desktop",
   },
   {
     name: "Wedding site",
@@ -28,9 +30,19 @@ const cardsData = [
     technologies: "React, Next.js, Tailwind, Netlify",
     git: "https://github.com/Homvi/next-wedding",
     live: "https://brigieskalmi.com/",
-    speed: "https://pagespeed.web.dev/report?url=https%3A%2F%2Fbrigieskalmi.com%2F&form_factor=desktop"
+    speed:
+      "https://pagespeed.web.dev/report?url=https%3A%2F%2Fbrigieskalmi.com%2F&form_factor=desktop",
   },
-]
+  {
+    name: "Luxcode",
+    desc: "Hobby project made for my future business.",
+    technologies: "React, Git, Tailwind, BrowserRouter",
+    git: "https://github.com/Homvi/luxcode",
+    live: "https://luxcode.dev/",
+    speed:
+      "https://pagespeed.web.dev/analysis/https-luxcode-dev/qkgacrtoyi?form_factor=desktop",
+  },
+];
 
 export default function Home() {
   const [isBorderOnNav, setIsBorderOnNav] = useState(false);
@@ -45,10 +57,6 @@ export default function Home() {
     });
   }, []);
 
-  /*   const addBorderToNavbar = () =>{
-    
-  } */
-
   return (
     <>
       <Head>
@@ -56,21 +64,31 @@ export default function Home() {
         <meta name="description" content="adakin's official portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="bg-black text-white pt-20 overflow-x-hidden">
+      <div className="pt-20 overflow-x-hidden text-white bg-black">
         <Navbar isBorder={isBorderOnNav} />
-        <div className="flex flex-col justify-center px-8  min-h-screen bg-black">
-          <div className="flex flex-col justify-center items-start max-w-2xl border-gray-700 mx-auto pb-16">
+        <div className="flex flex-col justify-center min-h-screen px-8 bg-black">
+          <div className="flex flex-col items-start justify-center max-w-2xl pb-16 mx-auto border-gray-700">
             <Introduction />
             <h3
               id="works"
-              className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-left w-full text-white"
+              className="w-full mb-6 text-2xl font-bold tracking-tight text-left text-white md:text-4xl"
             >
               Works and Projects
             </h3>
             <div className="overflow-x-scroll md:overflow-x-hidden relative left-[-30px] flex justify-start px-5  w-screen">
               <div className="flex justify-start">
                 {cardsData.map((card, id) => {
-                  return <Card key={`card_${id}`} name={card.name} desc={card.desc} speed={card.speed} technologies={card.technologies} git={card.git} live={card.live} />
+                  return (
+                    <Card
+                      key={`card_${id}`}
+                      name={card.name}
+                      desc={card.desc}
+                      speed={card.speed}
+                      technologies={card.technologies}
+                      git={card.git}
+                      live={card.live}
+                    />
+                  );
                 })}
               </div>
             </div>
